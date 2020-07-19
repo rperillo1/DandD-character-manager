af9616a70055f24a3744b1e45c0eb34bb60844c5
Robert Perillo <perillo.robert@gmail.com>
2020-07-19 15:47:57 -0600

    update stat and saving throws components

--- src/components/SavingThrowsCard/SavingThrowsCard.jsx
+++ src/components/SavingThrowsCard/SavingThrowsCard.jsx
@@ -2,10 +2,51 @@ import React from 'react';
 
 
 
-function SavingThrowsCard() {
+function SavingThrowsCard({ character }) {
+
+    const modifierObj = {
+        1: -5,
+        2: -4,
+        3: -4,
+        4: -3,
+        5: -3,
+        6: -2,
+        7: -2,
+        8: -1,
+        9: -1,
+        10: 0,
+        11: 0,
+        12: 1,
+        13: 1,
+        14: 2,
+        15: 2,
+        16: 3,
+        17: 3,
+        18: 4,
+        19: 4,
+        20: 5,
+        21: 5,
+        22: 6,
+        23: 6,
+        24: 7,
+        25: 7,
+        26: 8,
+        27: 8,
+        28: 9,
+        29: 9,
+        30: 10
+    }
+
+
     return (
         <div className="saving-throws-card">
-            saving thorws
+            Saving Throws:
+            <p>Strength:{character.savingThrows.includes('strength') ? <span>{modifierObj[character.stats.str] + character.profBonus}</span> : <span>{modifierObj[character.stats.str]}</span> }</p>
+            <p>Dexterity:{character.savingThrows.includes('dexterity') ? <span>{modifierObj[character.stats.dex] + character.profBonus}</span> : <span>{modifierObj[character.stats.dex]}</span> }</p>
+            <p>Constitution:{character.savingThrows.includes('constitution') ? <span>{modifierObj[character.stats.con] + character.profBonus}</span> : <span>{modifierObj[character.stats.con]}</span> }</p>
+            <p>Intelligence:{character.savingThrows.includes('intelligence') ? <span>{modifierObj[character.stats.int] + character.profBonus}</span> : <span>{modifierObj[character.stats.int]}</span>}</p>
+            <p>Wisdom:{character.savingThrows.includes('wisdom') ? <span>{modifierObj[character.stats.wis] + character.profBonus}</span> : <span>{modifierObj[character.stats.wis]}</span>}</p>
+            <p>Charisma:{character.savingThrows.includes('charisma') ? <span>{modifierObj[character.stats.char] + character.profBonus}</span> : <span>{modifierObj[character.stats.char]}</span> }</p>
         </div>
     )
 }
--- src/components/StatsCard/StatsCard.jsx
+++ src/components/StatsCard/StatsCard.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
-function StatsCard() {
+function StatsCard({ character }) {
     return (
         <div className='stats-card'>
-            stats card
+            <div className='flex-container'>
+                <p>Str: {character.stats.str}</p>
+            </div>
+            <div className='flex-container'>
+                <p>Dex: {character.stats.dex}</p>
+            </div>
+            <div className='flex-container'>
+                <p>Con: {character.stats.con}</p>
+            </div>
+            <div className='flex-container'>
+                <p>Int: {character.stats.int}</p>
+            </div>
+            <p>Wis: {character.stats.wis}</p>
+            <div className='flex-container'>
+                <p>Char: {character.stats.char}</p>
+            </div>
         </div>
     )
 }
